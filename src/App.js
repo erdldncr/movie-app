6450ae4c672ac5f5a4e02f7eafd1a3229d750c4f
erdldncr <erdldncr@gmail.com>
2021-05-30 18:04:00 +0100

    complete project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+//Created by [rce] snippet.
+import React, { Component } from "react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import MoviePage from "./components/pages/MoviePage";
+import { Container } from "semantic-ui-react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import NewMoviePage from "./components/pages/NewMoviePage"
+import HomePage from "./components/pages/HomePage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Container text>
+          <Route exact path="/" component={HomePage}></Route>          
+          <Route exact path="/movies" component={MoviePage}></Route>          
+          <Route exact path="/movies/new" component={NewMoviePage}></Route>
+          <Route exact path="/movie/:id" component={NewMoviePage}></Route>
+        </Container>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'semantic-ui-css/semantic.min.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore,applyMiddleware } from "redux";
+import rootReducer from './reducers/rootReducer'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from "react-redux";
+import { BrowserRouter } from 'react-router-dom';
+import logger from 'redux-logger';
+import promise from 'redux-promise-middleware'
+
+const store = createStore(rootReducer,
+  composeWithDevTools(applyMiddleware(thunk,promise,logger)))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  // </React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
